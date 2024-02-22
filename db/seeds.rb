2d1b88aee83c58a03cac345ae77b4fe17e7129ce
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-02-22 11:09:45 +0100

    corrected img path

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 require "json"
 require "open-uri"
 
+Movie.destroy_all
+
 url = "https://tmdb.lewagon.com/movie/top_rated"
 user_serialized = URI.open(url).read
 movies = JSON.parse(user_serialized)["results"]
 puts "Starting"
 movies.each do |movie|
-  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: movie["poster_path"], rating: movie["vote_average"])
+  Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original/#{movie["poster_path"]}", rating: movie["vote_average"])
 end
 puts "finishing"
