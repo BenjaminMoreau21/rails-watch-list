3e109cc787d93327480e404a0ae70863c6039a2f
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-02-22 17:33:20 +0100

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :lists do
+  root "lists#index"
+  resources :lists, except: :index do
     resources :bookmarks, only: [:create]
   end
   delete "bookmarks/:id", to: "bookmarks#destroy", as: "bookmark"
