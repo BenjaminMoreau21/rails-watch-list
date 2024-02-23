b8e70962247e482640a8930ee2ec754c9904b1dd
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-02-23 16:15:20 +0100

    added validation for rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :list
   validates :comment, length: { minimum: 6 }
   validates :comment, :rating, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
