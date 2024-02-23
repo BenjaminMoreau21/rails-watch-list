60431c2b5d49268e5f745b2b9c7b2b7de0752458
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-02-23 11:52:53 +0100

    redirected to root when destroy list and active storage implemented

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,12 +25,12 @@ class ListsController < ApplicationController
   def destroy
     @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
