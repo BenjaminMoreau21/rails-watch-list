55761c376488e3840c0172e5bcff1c838f4b95d4
Benjamin Moreau <benjamin.moreau49@gmail.com>
2024-02-25 12:57:04 +0100

    add list in index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,6 +2,7 @@ class ListsController < ApplicationController
 
   def index
     @lists = List.all
+    @list = List.new
   end
 
   def show
@@ -16,10 +17,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
+    @lists = List.all
     if @list.save
       redirect_to list_path(@list)
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
